51dcfa4fc59f8b31fa8a9c8d01e48ac5035d0178
LucasSu520 <suChristian0614@gmail.com>
2020-12-31 15:03:03 +0800

    add two feature  pull down to refresh and pull up to laod more

--- lib/pages/categoryPage.dart
+++ lib/pages/categoryPage.dart
@@ -8,12 +8,22 @@ class CategoryPage extends StatefulWidget {
 }
 
 class _CategoryPageState extends State<CategoryPage> {
-
   PersonList pList;
+  ScrollController _scrollController;
+
 
   @override
   void initState() {
     pList = new PersonList(data: []);
+    _scrollController = new ScrollController();
+
+    //this purpose of the method is to add the data to the list when the list is long
+    _scrollController.addListener(() {
+      if (_scrollController.position.pixels ==
+          _scrollController.position.maxScrollExtent && pList.data.length>20 ) {
+        getPersonList();
+      }
+    });
     super.initState();
   }
 
@@ -31,31 +41,37 @@ class _CategoryPageState extends State<CategoryPage> {
                 },
                 // ignore: missing_return
               )
-            : ListView.builder(
-                itemBuilder: (context, index) {
-                  if (index + 2 == pList.data.length) {
-                    getPersonList();
-                  }
-                  return Card(
-                    child: Row(
-                      children: [
-                        Icon(Icons.person),
-                        SizedBox(
-                          width: 20,
-                        ),
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text("姓名：" + pList.data[index].name),
-                            Text("学号：" + pList.data[index].idNum),
-                            Text("年龄：" + pList.data[index].age)
-                          ],
-                        )
-                      ],
-                    ),
-                  );
-                },
-                itemCount: pList.data.length,
+            : RefreshIndicator(
+                onRefresh: _handleRefresh,
+                child: ListView.builder(
+                  controller: this._scrollController,
+                  physics: AlwaysScrollableScrollPhysics(),
+                  itemBuilder: (context, index) {
+                    //if the number of data is small, this method will add data to the list, when the list is short
+                    if(index+2==pList.data.length&&pList.data.length<20){
+                      getPersonList();
+                    }
+                    return Card(
+                      child: Row(
+                        children: [
+                          Icon(Icons.person),
+                          SizedBox(
+                            width: 20,
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text("姓名：" + pList.data[index].name),
+                              Text("学号：" + pList.data[index].idNum),
+                              Text("年龄：" + pList.data[index].age)
+                            ],
+                          )
+                        ],
+                      ),
+                    );
+                  },
+                  itemCount: pList.data.length,
+                ),
               ));
   }
 
@@ -65,5 +81,14 @@ class _CategoryPageState extends State<CategoryPage> {
     setState(() {
       pList.data.addAll(newList.data);
     });
+  }
+
+  //achieve the feature when  pull down to refresh the list
+  Future<Null> _handleRefresh() async {
+    await Future.delayed(Duration(seconds: 1));
+    setState(() {
+      this.pList.data = this.pList.data.reversed.toList();
+    });
+    return null;
   }
 }
